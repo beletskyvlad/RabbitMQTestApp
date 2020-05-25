a555a3babd8fdc03b8d9ea6641f7a438f35879b6
beletskyvlad <beletsky.vlad@apriorit.com>
2020-05-25 20:18:34 +0300

    Renamed sender to publisher, implemented simple Publisher-Subscriber example

---
+++ Publisher/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static readonly Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Press [enter] to continue...");
+            Console.ReadLine();
+            var messagesSentCount = 0;
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes("test");
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                Console.WriteLine("Press [escape] to stop publishing messages.");
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        channel.BasicPublish(exchange: "",
+                         routingKey: "task_queue",
+                         basicProperties: properties,
+                         body: body);
+                        Console.WriteLine($"Messages sent: {++messagesSentCount}");
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);
+                        Thread.Sleep(300);
+                    }
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Press [enter] to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 6, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                var messagesReceived = 0;
+                consumer.Received += (sender, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    // Note: it is possible to access the channel via
+                    //       ((EventingBasicConsumer)sender).Model here
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    Console.WriteLine($"Messages received: {++messagesReceived}");
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Thread.Sleep(500);
+                };
+                channel.BasicConsume(queue: "task_queue",
+                                     autoAck: false,
+                                     consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
